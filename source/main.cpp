8d674198d8a2d8827608f24fe33af55b22645ff9
crystal / ue <mfihy1@gmail.com>
2024-02-18 02:31:50 -0500

    send test http request

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,35 @@
 #include "main.h"
+#include <curl/curl.h>
+#include <malloc.h>
+#include <stdarg.h>
+
+#define SOC_ALIGN       0x1000
+#define SOC_BUFFERSIZE  0x100000
+
+static u32 *SOC_buffer = NULL;
+s32 sock = -1, csock = -1;
+
+void failExit(const char *fmt, ...) {
+	if(sock>0) close(sock);
+	if(csock>0) close(csock);
+
+	va_list ap;
+
+	printf(CONSOLE_RED);
+	va_start(ap, fmt);
+	vprintf(fmt, ap);
+	va_end(ap);
+	printf(CONSOLE_RESET);
+	printf("\nPress B to exit\n");
+
+	while (aptMainLoop()) {
+		gspWaitForVBlank();
+		hidScanInput();
+
+		u32 kDown = hidKeysDown();
+		if (kDown & KEY_B) exit(0);
+	}
+}
 
 int main() {
 	gfxInitDefault();
@@ -8,14 +39,48 @@ int main() {
 	Output output;
 	Engine::Core core(&output);
 
+    // init socket service
+    int ret;
+    SOC_buffer = (u32*)memalign(SOC_ALIGN, SOC_BUFFERSIZE);
+
+	if(SOC_buffer == NULL)
+		failExit("memalign: failed to allocate\n");
+	if ((ret = socInit(SOC_buffer, SOC_BUFFERSIZE)) != 0)
+    	failExit("socInit: 0x%08X\n", (unsigned int)ret);
+
+    static std::string test_url = "https://catfact.ninja/fact";
+    output.print("Sending HTTP Request...");
+
+    CURL *curl;
+    CURLcode result;
+    curl = curl_easy_init();
+    std::stringstream debug;
+
+    if (curl == NULL) {
+        output.print("HTTP request failed.");
+    }
+    else {
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+        curl_easy_setopt(curl, CURLOPT_URL, test_url.c_str());
+        result = curl_easy_perform(curl);
+
+        if (result != CURLE_OK) {
+            debug << "Error: " << curl_easy_strerror(result);
+            output.print(debug.str());
+        }
+        else
+            curl_easy_cleanup(curl);
+    }
+
 	while (aptMainLoop()){
 		hidScanInput();
 		u32 downEvent = hidKeysDown();
 		u32 heldEvent = hidKeysHeld();
 		if ((downEvent & KEY_START) || (heldEvent & KEY_START)){
+            socExit();
 			break;
 		}
-
+        
 		gspWaitForVBlank();
 	}
 	
