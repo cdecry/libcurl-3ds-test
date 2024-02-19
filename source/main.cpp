180596eec0983b7656512414ca492b6cfca1635b
crystal / ue <mfihy1@gmail.com>
2024-02-18 20:35:29 -0500

    add socket service class

--- source/main.cpp
+++ source/main.cpp
@@ -1,38 +1,25 @@
 #include "main.h"
-#include <stdarg.h>
-
-#define SCREEN_WIDTH  400
-#define SCREEN_HEIGHT 240
-#define SOC_ALIGN       0x1000
-#define SOC_BUFFERSIZE  0x100000
-
-static u32 *SOC_buffer = NULL;
-s32 sock = -1, csock = -1;
-
-void failExit(const char *fmt, ...);
-void initSocketService();
 
 int main() {
 	gfxInitDefault();
 
     Output output;
 	Engine::Core core(&output);
+    SocketService socketService;
     GUI gui;
 
 	consoleSelect(consoleInit(GFX_BOTTOM, NULL));
-    initSocketService();
+    socketService.initSocketService();
     
     std::string url = "https://example.com";
     std::stringstream s;
 
     output.print("Testing GUI.\n");
 
-    // s << sendHTTPRequest(url, &output);
-    // output.print("Response: " + s.str());
     std::string response = sendHTTPRequest(url, &output);
+    
     char *cstr = new char[response.length() + 1];
     strcpy(cstr, response.c_str());
-    
     gui.addStaticTextElement(cstr);
     gui.sceneInit();
 
@@ -78,36 +65,4 @@ int main() {
     gui.sceneExit();
 	gfxExit();
 	return 0;
-}
-
-void failExit(const char *fmt, ...) {
-	if(sock>0) close(sock);
-	if(csock>0) close(csock);
-
-	va_list ap;
-
-	printf(CONSOLE_RED);
-	va_start(ap, fmt);
-	vprintf(fmt, ap);
-	va_end(ap);
-	printf(CONSOLE_RESET);
-	printf("\nPress B to exit\n");
-
-	while (aptMainLoop()) {
-		gspWaitForVBlank();
-		hidScanInput();
-
-		u32 kDown = hidKeysDown();
-		if (kDown & KEY_B) exit(0);
-	}
-}
-
-void initSocketService() {
-    int ret;
-    SOC_buffer = (u32*)memalign(SOC_ALIGN, SOC_BUFFERSIZE);
-
-	if(SOC_buffer == NULL)
-		failExit("memalign: failed to allocate\n");
-	if ((ret = socInit(SOC_buffer, SOC_BUFFERSIZE)) != 0)
-    	failExit("socInit: 0x%08X\n", (unsigned int)ret);
 }
--- source/main.h
+++ source/main.h
@@ -8,6 +8,7 @@
 #include "utility/output.h"
 #include "utility/gui.h"
 #include "network/crl.h"
+#include "network/socketService.h"
 #include "engine/core.h"
 
 #endif
